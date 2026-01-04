08c12577331259c0a11a6f258bbc862c09271828
Maxwell Renke <maxrenke@gmail.com>
2026-01-03 19:30:06 -0800

    [Q1] [Time Beats: 100.00%] [Memory Beats: 90.69%] - LeetPush

--- Q1.-Build-an-Array-With-Stack-Operations.py
+++ Q1.-Build-an-Array-With-Stack-Operations.py
@@ -1,18 +1,16 @@
 1class Solution:
 2    def buildArray(self, target: List[int], n: int) -> List[str]:
 3        ops = []
-4        t = 0  # index in target
+4        prev = 0
 5
-6        for num in range(1, n + 1):
-7            if t == len(target):
-8                break
-9
-10            ops.append("Push")
+6        for x in target:
+7            # Push+Pop for all numbers between prev and x
+8            for _ in range(x - prev - 1):
+9                ops.append("Push")
+10                ops.append("Pop")
 11
-12            if num == target[t]:
-13                t += 1
-14            else:
-15                ops.append("Pop")
+12            ops.append("Push")
+13            prev = x
+14
+15        return ops
 16
-17        return ops
-18
